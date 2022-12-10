b2bc6d47c132601f37f12660f68214e5cb41bda0
Marcelo Souza <contato.marcelooficial@gmail.com>
2022-12-09 21:43:59 -0300

    created menu hamburger & fix resonsive cellphone

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 document.getElementById("searchButton").onclick = function() {
   var obj = document.getElementById("inputtext");
-  obj.style.removeProperty("display");
+  if (obj.style.display == 'block') {
+    obj.style.display = 'none' 
+  } else {
+    obj.style.display = 'block'
+  }
+}
+
+document.getElementById("menuHamburger").onclick = function() {
+  var obj = document.getElementById("navbar");
+  if (obj.style.display == 'block') {
+    obj.style.display = 'none' 
+  } else {
+    obj.style.display = 'block'
+  }
 }
